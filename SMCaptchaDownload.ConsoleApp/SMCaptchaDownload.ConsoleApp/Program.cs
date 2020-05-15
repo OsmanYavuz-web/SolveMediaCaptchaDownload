63a4bb9a87c0d4bd0f35820f3f4d6aebf6d1a15d
OsmanYavuz-web <58117722+OsmanYavuz-web@users.noreply.github.com>
2020-05-15 23:53:10 +0300

    Add

---
+++ SMCaptchaDownload.ConsoleApp/SMCaptchaDownload.ConsoleApp/Program.cs
@@ -0,0 +1,60 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Drawing;
+using System.Threading;
+using OpenQA.Selenium.Chrome;
+using Tiny.RestClient;
+
+namespace SMCaptchaDownload.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                // Sürücü ve ayar
+                ChromeOptions options = new ChromeOptions();
+                options.AddArgument("--window-size=800,600");
+                IWebDriver driver = new ChromeDriver(options);
+                
+                // Siteye Git
+                driver.Navigate().GoToUrl("https://tensorcrypto.website/etc/");
+
+                // Captcha'ya odaklanma
+                driver.FindElement(By.XPath("//input[@name='adcopy_response']"))
+                    .SendKeys("");
+
+                // Bekle
+                Thread.Sleep(2000);
+                
+
+                // Ekran kaydı al
+                Screenshot image = ((ITakesScreenshot)driver).GetScreenshot();
+                image.SaveAsFile(
+                    @"C:\Users\omnyvz\Desktop\test.png",
+                    ScreenshotImageFormat.Png);
+
+                // Ekran kaydından captcha bölümünü kes
+                Image img = Image.FromFile(@"C:\Users\omnyvz\Desktop\test.png");
+                Bitmap bmpImage = new Bitmap(img);
+                Bitmap bmpCrop = bmpImage.Clone(
+                    new Rectangle(
+                        235, 57, // captcha konumu
+                        300, 150 // captcha boyutu
+                        ), bmpImage.PixelFormat);
+
+                // Captcha 
+                bmpCrop.Save(@"C:\Users\omnyvz\Desktop\captcha.jpg");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
